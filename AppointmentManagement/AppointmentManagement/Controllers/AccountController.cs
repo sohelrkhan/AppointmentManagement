0d70f356a2639d796fa96e1fdacaa968bbc968e0
Md. Sohel Rana Khan <sohelrkhan97@gmail.com>
2021-11-24 18:02:07 +0600

    7.2 Add LoginVM Done

---
+++ AppointmentManagement/AppointmentManagement/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppointmentManagement.Models;
+
+namespace AppointmentManagement.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
