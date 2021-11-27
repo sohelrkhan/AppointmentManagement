a10a80198c4176df28fcbfc6d981cca4200683dd
Md. Sohel Rana Khan <sohelrkhan97@gmail.com>
2021-11-27 16:11:11 +0600

    8 Section Done

--- AppointmentManagement/AppointmentManagement/wwwroot/js/script-custom-calendar.js
+++ AppointmentManagement/AppointmentManagement/wwwroot/js/script-custom-calendar.js
@@ -1,4 +1,11 @@
-﻿$(document).ready(function () {
+﻿var routeURL = location.protocol + "//" + location.host;
+
+$(document).ready(function () {
+	$("#appointmentDate").kendoDateTimePicker({
+		value: new Date(),
+		dateInput: false
+	});
+
 	InitializeCalendar();
 });
 
@@ -34,4 +41,57 @@ function onShowModal(obj, isEventDetail) {
 
 function onCloseModal() {
 	$("#appointmentInput").modal("hide");
+}
+
+function onSubmitForm() {
+	if (checkValidation()) {
+		var requestData = {
+			Id: parseInt($("#id").val()),
+			Tittle: $("#title").val(),
+			Description: $("#description").val(),
+			StartDate: $("#appointmentDate").val(),
+			Duration: $("#duration").val(),
+			DoctorId: $("#doctorId").val(),
+			PatientId: $("#patientId").val()
+		};
+
+		$.ajax({
+			url: routeURL + '/api/Appointment/SaveCalendarData',
+			type: 'POST',
+			data: JSON.stringify(requestData),
+			contentType: 'application/json',
+			success: function(response) {
+				if (response.status === 1 || response.status === 2) {
+					$.notify(response.message, "success");
+					onCloseModal();
+				} else {
+					$.notify(response.message, "error");
+				}
+			},
+			error: function(xhr) {
+				$.notify("Error", "error");
+			}
+		});
+	}
+}
+
+function checkValidation() {
+	var isValid = true;
+	if ($("#title").val() === undefined || $("#title").val() === "") {
+		isValid = false;
+		$("#title").addClass('error');
+	}
+	else {
+		$("#title").removeClass('error');
+	}
+
+	if ($("#appointmentDate").val() === undefined || $("#appointmentDate").val() === "") {
+		isValid = false;
+		$("#appointmentDate").addClass('error');
+	}
+	else {
+		$("#appointmentDate").removeClass('error');
+	}
+
+	return isValid;
 }
